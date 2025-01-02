85bd764987cb2e7820b7ddd3def1a4acb5d91735
vassilidev <vassilidevnet@gmail.com>
2025-01-02 14:20:31 +0100

    feat: rework landing page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,44 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $projects = config('projects');
+    $projects = [
+        [
+            'title'       => 'TFA - The Forge Agency 🚀',
+            'description' => 'Agence spécialisée dans le développement web et la création d’applications sur mesure. Transformez vos idées en succès numériques.',
+            'url'         => 'https://the-forge.agency',
+            'image'       => '/tfa.png',
+        ],
+        [
+            'title'       => 'Site éclair ⚡️',
+            'description' => 'Solution clé en main pour créer votre site vitrine en 5 jours, avec un prix fixe et tout inclus.',
+            'url'         => 'https://siteeclair.fr',
+            'image'       => '/siteeclair.png',
+        ],
+        [
+            'title'       => 'Vassili IT 💻',
+            'description' => 'Service premium de réparation informatique et montage PC à domicile, pour particuliers et professionnels.',
+            'url'         => 'https://linktr.ee/vassilidev',
+            'image'       => '/vassiliit.jpg',
+        ],
+        [
+            'title'       => 'TaxCut 📈',
+            'description' => 'Réduisez vos impôts simplement en quelques clics, faites votre simulation afin de connaître les solutions qui s\'offrent à vous',
+            'url'         => 'https://taxcut.fr',
+            'image'       => '/taxcut.png',
+        ],
+        [
+            'title'       => 'La Boîte du Temps ⌚',
+            'description' => 'Boutique en ligne dédiée aux montres, textiles et accessoires horlogers. Découvrez des produits uniques.',
+            'url'         => 'https://laboitedutemps.com',
+            'image'       => '/lbdt.png',
+        ],
+        [
+            'title'       => 'Chope Ton Resto! 🍽️',
+            'description' => 'Trouvez les meilleurs restaurants près de chez vous grâce à une recherche intuitive et des avis authentiques.',
+            'url'         => 'https://chopetonresto.fr',
+            'image'       => 'https://chopetonresto.fr/assets/img.png',
+        ],
+    ];
 
     return view('welcome', compact('projects'));
 });
