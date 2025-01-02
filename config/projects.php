04c682dd89db968944c2f23ad86c8d33353ba630
vassilidev <vassilidevnet@gmail.com>
2025-01-02 14:19:52 +0100

    feat: rework landing page

--- config/projects.php
+++ config/projects.php
@@ -1,37 +1,35 @@
 <?php
 
-$url = env('APP_URL');
-
 return [
     [
         'title'       => 'TFA - The Forge Agency 🚀',
         'description' => 'Agence spécialisée dans le développement web et la création d’applications sur mesure. Transformez vos idées en succès numériques.',
         'url'         => 'https://the-forge.agency',
-        'image'       => $url . '/tfa.png',
+        'image'       => '/tfa.png',
     ],
     [
         'title'       => 'Site éclair ⚡️',
         'description' => 'Solution clé en main pour créer votre site vitrine en 5 jours, avec un prix fixe et tout inclus.',
         'url'         => 'https://siteeclair.fr',
-        'image'       => $url . '/siteeclair.png',
+        'image'       => '/siteeclair.png',
     ],
     [
         'title'       => 'Vassili IT 💻',
         'description' => 'Service premium de réparation informatique et montage PC à domicile, pour particuliers et professionnels.',
         'url'         => 'https://linktr.ee/vassilidev',
-        'image'       => $url . '/vassiliit.jpg',
+        'image'       => '/vassiliit.jpg',
     ],
     [
         'title'       => 'TaxCut 📈',
         'description' => 'Réduisez vos impôts simplement en quelques clics, faites votre simulation afin de connaître les solutions qui s\'offrent à vous',
         'url'         => 'https://taxcut.fr',
-        'image'       => $url . '/taxcut.png',
+        'image'       => '/taxcut.png',
     ],
     [
         'title'       => 'La Boîte du Temps ⌚',
         'description' => 'Boutique en ligne dédiée aux montres, textiles et accessoires horlogers. Découvrez des produits uniques.',
         'url'         => 'https://laboitedutemps.com',
-        'image'       => $url . '/lbdt.png',
+        'image'       => '/lbdt.png',
     ],
     [
         'title'       => 'Chope Ton Resto! 🍽️',
