f567f64fe2f081d900b1da0877383c9cf484dd6c
vassilidev <vassilidevnet@gmail.com>
2025-01-02 14:18:24 +0100

    feat: rework landing page

--- config/projects.php
+++ config/projects.php
@@ -1,35 +1,37 @@
 <?php
 
+$url = env('APP_URL');
+
 return [
     [
         'title'       => 'TFA - The Forge Agency 🚀',
         'description' => 'Agence spécialisée dans le développement web et la création d’applications sur mesure. Transformez vos idées en succès numériques.',
         'url'         => 'https://the-forge.agency',
-        'image'       => asset('tfa.png')
+        'image'       => $url . '/tfa.png',
     ],
     [
         'title'       => 'Site éclair ⚡️',
         'description' => 'Solution clé en main pour créer votre site vitrine en 5 jours, avec un prix fixe et tout inclus.',
         'url'         => 'https://siteeclair.fr',
-        'image'       => asset('siteeclair.png'),
+        'image'       => $url . '/siteeclair.png',
     ],
     [
         'title'       => 'Vassili IT 💻',
         'description' => 'Service premium de réparation informatique et montage PC à domicile, pour particuliers et professionnels.',
         'url'         => 'https://linktr.ee/vassilidev',
-        'image'       => asset('vassiliit.jpg'),
+        'image'       => $url . '/vassiliit.jpg',
     ],
     [
         'title'       => 'TaxCut 📈',
         'description' => 'Réduisez vos impôts simplement en quelques clics, faites votre simulation afin de connaître les solutions qui s\'offrent à vous',
         'url'         => 'https://taxcut.fr',
-        'image'       => asset('taxcut.png'),
+        'image'       => $url . '/taxcut.png',
     ],
     [
         'title'       => 'La Boîte du Temps ⌚',
         'description' => 'Boutique en ligne dédiée aux montres, textiles et accessoires horlogers. Découvrez des produits uniques.',
         'url'         => 'https://laboitedutemps.com',
-        'image'       => asset('lbdt.png'),
+        'image'       => $url . '/lbdt.png',
     ],
     [
         'title'       => 'Chope Ton Resto! 🍽️',
